d5579a172ab98752350169d7cc03a07618f63c62
Shandy Siswandi <shandysiswandi@gmail.com>
2020-11-16 18:22:03 +0700

    remove comment

--- simple-client-server-app/server.js
+++ simple-client-server-app/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const fs = require("fs");
 const bodyParser = require("body-parser");
-// const util = require("util");
 
 const pathLog = __dirname + "/server.log";
 fs.unlinkSync(pathLog);
@@ -9,32 +8,6 @@ const log_file = fs.createWriteStream(pathLog, { flags: "w" });
 
 const port = 3000;
 const app = express();
-
-Date.prototype.toIsoString = function () {
-  var tzo = -this.getTimezoneOffset(),
-    dif = tzo >= 0 ? "+" : "-",
-    pad = function (num) {
-      var norm = Math.floor(Math.abs(num));
-      return (norm < 10 ? "0" : "") + norm;
-    };
-  return (
-    this.getFullYear() +
-    "-" +
-    pad(this.getMonth() + 1) +
-    "-" +
-    pad(this.getDate()) +
-    "T" +
-    pad(this.getHours()) +
-    ":" +
-    pad(this.getMinutes()) +
-    ":" +
-    pad(this.getSeconds()) +
-    dif +
-    pad(tzo / 60) +
-    ":" +
-    pad(tzo % 60)
-  );
-};
 
 const msgFunc = (req) =>
   `[${new Date().toISOString()}] Success: ${
